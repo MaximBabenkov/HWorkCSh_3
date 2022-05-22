d95c4247a6ea0aff01da916c95df89bb0e20db09
MaximBabenkov <lostbass@mail.ru>
2022-05-22 11:35:01 +0300

    Task_5

---
+++ Task_5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Write("Enter your number: ");
+int a = int.Parse(Console.ReadLine()??"0");
+int sum = 0;
+while(a>0)
+{
+    sum=sum+a%10;
+    a=a/10;
+}
+Console.Write($"The sum of digits of this number: {sum}");
