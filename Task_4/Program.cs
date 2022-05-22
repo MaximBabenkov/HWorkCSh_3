4b2246b09a467cf05c86844404c22b3cb0161797
MaximBabenkov <lostbass@mail.ru>
2022-05-22 11:27:09 +0300

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возвести число a в натуральную степень b, используя цикл
+Console.Write("Enter your number: ");
+int a = int.Parse(Console.ReadLine()??"0");
+Console.Write("Enter the desired power of number: ");
+int b = int.Parse(Console.ReadLine()??"0");
+int result = 1;
+for (int i=1;i<=b;i++)
+{
+    result=result*a;
+}
+Console.Write($"The result of raising to the power: {result}");
