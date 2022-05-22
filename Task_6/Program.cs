926809b4b8214b35fd7c71832a2af9729b8f1766
MaximBabenkov <lostbass@mail.ru>
2022-05-22 11:41:12 +0300

    Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Enter your number: ");
+int n = int.Parse(Console.ReadLine()??"0");
+int a = 1;
+while (a<=n)
+{
+    if (a%2==0)
+    {
+        Console.Write($"{a*a*a} ");
+        a++;
+    }
+    else
+    a++;    
+}
